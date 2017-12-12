dc91816fda33dc3e278520e925555c2ef7194f2b
narendra67 <mvnarendrareddy@gmail.com>
2017-12-12 23:49:38 +0530

    state declared and input is changed to control component

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,11 +2,27 @@ import React from "react";
 import {Component} from "react";
 
 class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state= {
+            term:"Start"
+        }
+
+
+    }
+
+
+
     render(){
         return(
-            <input  onChange={event => console.log(event.target.value)}/>
+            <div>
+                <input value={this.state.term}
+                       onChange={event => this.setState({term:event.target.value})}/>
+            </div>
         )
     }
+
 }
 
 
